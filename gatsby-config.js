f065e47953cd0b872b3773b8274fa66b70d4b6c2
Arthur Moreno <morenorarthur@gmail.com>
2025-04-12 02:34:36 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Life Simulation Game`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
